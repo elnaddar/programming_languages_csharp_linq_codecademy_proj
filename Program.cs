90437332498ae6542a6b449249acf55db8f82802
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-03-30 13:17:26 +0200

    added comment for each task.

--- Program.cs
+++ Program.cs
@@ -14,9 +14,17 @@ namespace ProgrammingLanguages
               .Select(line => Language.FromTsv(line))
               .ToList();
 
+            /*
+                1. Let’s start by printing all of the languages:
+                   print each item in languages by calling its Prettify() method.
+            */
             foreach (Language l in languages)
                 Console.WriteLine(l.Prettify());
 
+            /*
+                2. Write a query that returns a string for each language. It should include the year, name, and chief developer of each language.
+                   Print each one to the console.
+            */
             foreach (var s in languages.Select(l => $"{l.Year}, {l.Name}, {l.ChiefDeveloper}"))
                 Console.WriteLine(s);
         }
